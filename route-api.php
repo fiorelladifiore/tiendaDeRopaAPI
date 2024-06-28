06ea6d4bc0b95d0efde7ab6b96e102dbc43e900c
Iriart <iriartmanuel81@gmail.com>
2024-06-28 16:31:22 -0300

    Route solucionado -products-

--- route-api.php
+++ route-api.php
@@ -15,6 +15,24 @@
     $router->addRoute('stores/:ID', 'PUT', 'storesApiController', 'updateStore');
     $router->addRoute('auth', 'POST', 'authApiController', 'login');
 
+    <?php
+    require_once('libs/Router.php');
+    require_once('app/controller/productsApiController.php');
+    
+
+    $router = new Router();
+
+    // GET http://localhost/proyectos/tabajoAPI/api/products
+    $router->addRoute('products', 'GET', 'productsApiController', 'getProducts');
+    // obtener producto, 1 solo por id
+    $router->addRoute('products/:ID', 'GET', 'productsApiController', 'getProduct');
+    // agregar un producto
+    $router->addRoute('product', 'POST', 'productsApiController', 'addProduct');
+    // eliminar un producto
+    $router->addRoute('product/:ID', 'DELETE', 'productsApiController', 'deleteProduct');
+    // editar producto
+    $router->addRoute('product/:ID', 'PUT', 'productsApiController', 'updateProduct');
+    
     $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
 
 
